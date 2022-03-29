24ffb8643c8ad4f7e624ea9ab2f4b8d5e4a0068d
Gupta <gaurav.bs.gupta@accenture.com>
2022-03-29 10:19:56 +0530

    refactor code tenureInYears is now calculated in separate method convertDaysToYears

--- src/SimpleInterestCalculator.java
+++ src/SimpleInterestCalculator.java
@@ -4,22 +4,26 @@ public class SimpleInterestCalculator {
 	private double calculateSimpleInterest(int principalAmount , double rateOfInterest, double tenureInYears) {
 		return principalAmount * rateOfInterest *tenureInYears / 100;
 	}
-	public double calculate(int principalAmount, int tenure) {
+	
+	private double convertDaysToYears(int tenureInDays) {
+		return tenureInDays / 365.0;
+	} 
+	public double calculate(int principalAmount, int tenureInDays) {
 		double rateOfInterest = 5.3;
-		double tenureInyears = tenure / 365.0;
+		double tenureInyears = convertDaysToYears(tenureInDays);
 		
-		if(tenure < 365) {
+		if(tenureInDays < 365) {
 			rateOfInterest = 5.2;
 		}
 		return calculateSimpleInterest(principalAmount, rateOfInterest, tenureInyears);
 		
 	}
 	
-	public double calculate(int principalAmount, int tenure,int age) {
+	public double calculate(int principalAmount, int tenureInDays,int age) {
 		double rateOfInterest = 5.3;
-		double tenureInyears = tenure / 365.0;
+		double tenureInyears = convertDaysToYears(tenureInDays);
 		
-		if(tenure < 365) {
+		if(tenureInDays < 365) {
 			rateOfInterest = 5.2;
 		}
 		 
